1852b10c3dbf4776bce864b2573ae490f4520e52
Aditya Raj <86474943+adityaraj5@users.noreply.github.com>
2023-11-15 12:49:02 +0530

    Update SpringJenkinsPipelineGroovyApplication.groovy
    another change

--- src/main/groovy/com/myapp/spring/SpringJenkinsPipelineGroovyApplication.groovy
+++ src/main/groovy/com/myapp/spring/SpringJenkinsPipelineGroovyApplication.groovy
@@ -3,6 +3,7 @@ package com.myapp.spring
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 //some changes done form Aditya's repository
+//another change
 @SpringBootApplication
 class SpringJenkinsPipelineGroovyApplication {
 
